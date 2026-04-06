63c16cf52580a1c20829892f00f58b56c0abbfbb
tinhha7949 <tinhha7949@gmail.com>
2026-04-07 06:36:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const LIMIT_1H  = 200
 const RR_THRESHOLD = 1.2
 const RISK_PER_TRADE = 0.005
 const ACCOUNT_BALANCE = 1000
-const MIN_VOL_15M = 300000
+const MIN_VOL_15M = 200000
 
 let isScanning = false
 let lastSignalTime = 0
